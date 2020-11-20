d0430b9f8efacff55d0a82647c6d843a12c5256d
xxx <xxx@users.noreply.github.com>
2020-11-20 13:35:09 -0700

    and were back!

--- lib/org.rb
+++ lib/org.rb
@@ -42,6 +42,6 @@ class Organizer
       f.write(self.text.value)
     }
     `emacs index.org --batch -f org-html-export-to-html --kill`
-    self.html.value = /<body>(.*)<\/body>/.match(File.read("index.html"))[1]
+    self.html.value = /<body>(.*)<\/body>/m.match(File.read("index.html"))[1]
   end
 end
