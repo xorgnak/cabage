4bb8b4a31fff0da3d84014b0c031e678feedf888
xxx <xxx@users.noreply.github.com>
2020-11-30 15:36:03 -0700

    better breakdown

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -1,27 +1,29 @@
+def sendSms t, m
+  th = {
+    to: t,
+    body: m
+  }
+  t = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
+  t.messages.create(th) 
+end
 class Twiml
   @@JOBS = {}
   @@PINS = {}
   @@BLOCKS = {
     :call => lambda { |r, h|
-      if !@@JOBS.has_key? h['From']
-        p = [rand(9), rand(9), rand(9), rand(9)]
-        @@PINS[p.join('')] = h['From']
-        @@JOBS[h['From']] = p.join('')
-      end
+        user_job h['From']
       r.gather(action: '/admin', timeout: 10) { |g|
         g.say(message: CONF['callcenter']['welcome'] + ", You will recieve a call about your task shortly" )
       }
       if h['From'] != CONF['owner']
-      th = {
-        to: CONF['owner'],
-        body: "[#{@@JOBS[h['From']]}] #{h['From']}"
-      }
-      t = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
-      t.messages.create(th)
+        sendSms(CONF['owner'], "[#{@@JOBS[h['From']]}] #{h['From']}")
       end
     },
     :sms => lambda { |r, h|
-      
+        user_job h['From']
+      if h['From'] != CONF['owner']
+        sendSms(CONF['owner'], "[#{@@JOBS[h['From']]}] #{h['From']} #{h['Body']}")
+      end 
     },
     :admin => lambda { |r,h|
       r.gather(action: '/admin') {|g| g.play(digits: '1p2p3') }
@@ -42,6 +44,13 @@ class Twiml
         @@BLOCKS[@b].call(resp, @h)
       end.to_s 
     end
+  end
+  def user_job u
+    if !@@JOBS.has_key? u
+      p = [rand(9), rand(9), rand(9), rand(9)]
+      @@PINS[p.join('')] = u
+      @@JOBS[u] = p.join('')
+    end 
   end
   def self.blocks
     @@BLOCKS
