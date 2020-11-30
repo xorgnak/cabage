17e7dab494764546b382129dc397977d80293542
xxx <xxx@users.noreply.github.com>
2020-11-30 15:48:40 -0700

    better cleaner

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -6,30 +6,38 @@ def sendSms t, m
   t = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
   t.messages.create(th) 
 end
+def user_job u
+  if Redis::HashKey.new("callcenter:pins")[u] == nil
+    p = [rand(9), rand(9), rand(9), rand(9)]
+    Redis::HashKey.new("callcenter:pins")[p.join('')] = u
+    Redis::HashKey.new("callcenter:jobs")[u] = p.join('')
+  end  
+end
 class Twiml
-  @@JOBS = {}
-  @@PINS = {}
+
   @@BLOCKS = {
     :call => lambda { |r, h|
-        user_job h['From']
+      user_job h['From']
       r.gather(action: '/admin', timeout: 10) { |g|
         g.say(message: CONF['callcenter']['welcome'] + ", You will recieve a call about your task shortly" )
       }
       if h['From'] != CONF['owner']
-        sendSms(CONF['owner'], "[#{@@JOBS[h['From']]}] #{h['From']}")
+        j = Redis::HashKey.new("callcenter:jobs")[h['From']]
+        sendSms(CONF['owner'], "[#{j}] #{h['From']}")
       end
     },
     :sms => lambda { |r, h|
-        user_job h['From']
+      user_job h['From']
       if h['From'] != CONF['owner']
-        sendSms(CONF['owner'], "[#{@@JOBS[h['From']]}] #{h['From']} #{h['Body']}")
+        j = Redis::HashKey.new("callcenter:jobs")[h['From']]
+        sendSms(CONF['owner'], "[#{j}] #{h['From']} #{h['Body']}")
       end 
     },
     :admin => lambda { |r,h|
       r.gather(action: '/admin') {|g| g.play(digits: '1p2p3') }
     },
     :call_status => lambda { |r,h|
-
+      
     }
   }
   def initialize b, h={}
@@ -44,13 +52,6 @@ class Twiml
         @@BLOCKS[@b].call(resp, @h)
       end.to_s 
     end
-  end
-  def user_job u
-    if !@@JOBS.has_key? u
-      p = [rand(9), rand(9), rand(9), rand(9)]
-      @@PINS[p.join('')] = u
-      @@JOBS[u] = p.join('')
-    end 
   end
   def self.blocks
     @@BLOCKS
