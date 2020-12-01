aa230fef4bf0dfb34e2eadb507e2601d9024bc86
xxx <xxx@users.noreply.github.com>
2020-12-01 12:08:14 -0700

    no logic in te block

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -8,31 +8,38 @@ module TWILIO
     @x = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
     @x.messages.create(th) 
   end
-  def self.set_job u
+  def set_job u
     if Redis::HashKey.new("callcenter:pins")[u] == nil
       p = [rand(9), rand(9), rand(9), rand(9)]
       Redis::HashKey.new("callcenter:pins")[p.join('')] = u
       Redis::HashKey.new("callcenter:jobs")[u] = p.join('')
     end
   end
-  def self.get_sms b, h={}
-    Twilio::TwiML::MessagingResponse.new do |resp|
-      if h['From'] != CONF['owner']
-        j = Redis::HashKey.new("callcenter:jobs")[@h['From']]
-        TWILIO.sendSms(CONF['owner'], "[#{j}] #{@h['From']} #{@h['Body']}")
+  def self.get_sms h={}
+    set_job h['From']
+#    if h['From'] != CONF['owner']
+      j = Redis::HashKey.new("callcenter:jobs")[h['From']]
+      TWILIO.sendSms(CONF['owner'], "[#{j}] #{h['From']} #{h['Body']}")
+      Twilio::TwiML::MessagingResponse.new do |resp|
         resp.message(body: "You will recieve a call about your task shortly.")
-      end 
-    end.to_s
+      end.to_s
+#    end
   end
-  def self.get_call b, h={}
+  def self.get_msg h={}
+    if @h['From'] != CONF['owner']
+      j = Redis::HashKey.new("callcenter:jobs")[h['From']]
+      TWILIO.sendSms(CONF['owner'], "[#{j}] #{h['From']}")
+    end  
     Twilio::TwiML::VoiceResponse.new do |resp|
-      resp.gather(action: '/call', timeout: 10) { |g|
-        g.say(message: CONF['callcenter']['welcome'] + ", You will recieve a call about your task shortly" )
+      resp.say(message: 'goodbye' )
+    end
+  end
+  def self.get_call h={}
+    set_job h['From']
+    Twilio::TwiML::VoiceResponse.new do |resp|
+      resp.gather(action: '/msg', timeout: 5) { |g|
+        g.say(message: CONF['callcenter']['welcome'] + ", please leave a message and ou will recieve a call about your task shortly" )
       }
-      if @h['From'] != CONF['owner']
-        j = Redis::HashKey.new("callcenter:jobs")[@h['From']]
-        TWILIO.sendSms(CONF['owner'], "[#{j}] #{@h['From']}")
-      end 
-    end.to_s 
+    end
   end
 end
