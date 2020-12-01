23fe5d0c7a2f8b702f54c7e31c91951e59a2c5a4
xxx <xxx@users.noreply.github.com>
2020-12-01 12:11:01 -0700

    upcase

--- lib/twilio.rb
+++ lib/twilio.rb
@@ -8,7 +8,7 @@ module TWILIO
     @x = Twilio::REST::Client.new(ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN'])
     @x.messages.create(th) 
   end
-  def set_job u
+  def self.set_job u
     if Redis::HashKey.new("callcenter:pins")[u] == nil
       p = [rand(9), rand(9), rand(9), rand(9)]
       Redis::HashKey.new("callcenter:pins")[p.join('')] = u
@@ -16,7 +16,7 @@ module TWILIO
     end
   end
   def self.get_sms h={}
-    set_job h['From']
+    TWILIO.set_job h['From']
 #    if h['From'] != CONF['owner']
       j = Redis::HashKey.new("callcenter:jobs")[h['From']]
       TWILIO.sendSms(CONF['owner'], "[#{j}] #{h['From']} #{h['Body']}")
@@ -35,7 +35,7 @@ module TWILIO
     end
   end
   def self.get_call h={}
-    set_job h['From']
+    TWILIO.set_job h['From']
     Twilio::TwiML::VoiceResponse.new do |resp|
       resp.gather(action: '/msg', timeout: 5) { |g|
         g.say(message: CONF['callcenter']['welcome'] + ", please leave a message and ou will recieve a call about your task shortly" )
