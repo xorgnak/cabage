209f61d20a8b414bd4fcdb6a6c8306702c1f29a7
xxx <xxx@users.noreply.github.com>
2020-12-02 13:06:24 -0700

    debug

--- lib/js.rb
+++ lib/js.rb
@@ -32,71 +32,74 @@ class JS
     t = []; 64.times { t << rand(16).to_s(16) }
     @tok = t.join('');
     ch = %[client.subscribe('#{CONF['network']}/#{@u}'); ]
-    return %[var token = '#{@tok}';                                                              
-var state = 0; 
-var pos_item = 0;
-var loc_num = 0;
-var ven = encodeURI(venmo({ txn: 'pay', recipients: '<%= @attr['venmo'] %>', amount: 20, note: "tips appreciated" }));
-var user;
-function onMessageArrived(message) {
-  var topic = message.destinationName;
-  var j = JSON.parse(message.payloadString);
-  if (topic == "time") {
-     if (j.hour < 10) {
-       $("#hour").text("0" + j.hour);
-     } else {
-       $("#hour").text(j.hour);
-     }
-     if (j.min < 10) {
-       $("#min").text("0" + j.min);
-     } else {
-       $("#min").text(j.min);
-     }
-  } else if (topic == "ping") {
-     sendMQTT();
-  } else if (j.action == 'w') {
-     $(j.element).html(j.payload);
-  } else if (j.action == 'a') { 
-     $(j.element).append(j.payload);
-  } else if (j.action == 'p') { 
-     $(j.element).prepend(j.payload);
-  }
-  console.log("onMessageArrived", topic, j);
-}
-function onFail() {
-client.connect();
-}
-function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  state++;
-  client.subscribe('ping');
-  client.subscribe('time');
-  #{ch}
-}
-// called when the client loses its connection                                         
-function onConnectionLost(responseObject) {                                            
-  if (responseObject.errorCode !== 0) {                                                
-state--;
-    console.log("onConnectionLost", responseObject);                      
-  }
-}
-function sendMQTT() {
-  var ua = $('form').serializeArray();
-  var ia = {};
-  $.map(ua, function(n, i){ ia[n['name']] = n['value']; });
-  ia['id'] = '#{@u}';
-  ia['state'] = state;
-  ia['token'] = token;
-  ia['user'] = user;
-  ia['venmo'] = ven;
-  console.log('sendMQTT',ia);
-  message = new Paho.MQTT.Message(JSON.stringify(ia));
-  message.destinationName = '#{CONF['network']}';
-  client.send(message);
-} 
-
-]
+	     return %[
+		 var token = '#{@tok}';
+		 var state = 0; 
+		 var pos_item = 0;
+		 var loc_num = 0;
+		 var ven;
+		 var user;
+		 function onMessageArrived(message) {
+		     var topic = message.destinationName;
+		     var j = JSON.parse(message.payloadString);
+		     if (topic == "time") {
+			 if (j.hour < 10) {
+			     $("#hour").text("0" + j.hour);
+			 } else {
+			     $("#hour").text(j.hour);
+			 }
+			 if (j.min < 10) {
+			     $("#min").text("0" + j.min);
+			 } else {
+			     $("#min").text(j.min);
+			 }
+		     } else if (topic == "ping") {
+			 sendMQTT();
+		     } else if (j.action == 'w') {
+			 $(j.element).html(j.payload);
+		     } else if (j.action == 'a') { 
+			 $(j.element).append(j.payload);
+		     } else if (j.action == 'p') { 
+			 $(j.element).prepend(j.payload);
+		     }
+		     console.log("onMessageArrived", topic, j);
+		 }
+		 function onFail() {
+		     client.connect();
+		 }
+		 function onConnect() {
+		     // Once a connection has been made, make a subscription and send a message.
+		     console.log("onConnect");
+		     state++;
+		     client.subscribe('ping');
+		     client.subscribe('time');
+		     var ti = { txn: 'pay', recipients: '<%= @attr['venmo'] %>', amount: 20, note: "tips appreciated" }
+		     ven = encodeURI(venmo(ti));
+#{ch}
+		 }
+		 // called when the client loses its connection                                         
+		 function onConnectionLost(responseObject) {
+		     if (responseObject.errorCode !== 0) {
+			 state--;
+			 console.log("onConnectionLost", responseObject);                      
+		     }
+		 }
+		 function sendMQTT() {
+		     var ua = $('form').serializeArray();
+		     var ia = {};
+		     $.map(ua, function(n, i){ ia[n['name']] = n['value']; });
+		     ia['id'] = '#{@u}';
+		     ia['state'] = state;
+		     ia['token'] = token;
+		     ia['user'] = user;
+		     ia['venmo'] = ven;
+		     console.log('sendMQTT',ia);
+		     message = new Paho.MQTT.Message(JSON.stringify(ia));
+		     message.destinationName = '#{CONF['network']}';
+		     client.send(message);
+		 } 
+		 
+	     ]
     
   end
   
