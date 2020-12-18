96a9eaffe85473c6cfa295b7fdf9cac50053cec3
pi <pi@nomadic>
2020-12-17 23:41:17 -0700

    all the new stuff

--- lib/handle.rb
+++ lib/handle.rb
@@ -61,3 +61,100 @@ module Handle
     end
   end 
 end
+
+def butler
+  Redis.new( host: "vango.me", db: 1 )
+end
+class Butler
+  def initialize
+    if File.exists? 'addressbook.json'
+      @book = JSON.parse(File.read('addressbook.json'))
+    else
+      @book = {}
+    end
+  end
+  def save!
+    File.open("addressbook.json", 'w') {|f| f.write(JSON.generate(@book)) }
+  end
+  def peek!
+    butler.monitor {|e| puts "#> #{e}" }
+  end
+  def my_id
+    if butler.get("butler:id") == nil || butler.get("butler:id") == ""
+      a = []; 8.times { a << rand(16).to_s(16) }
+      butler.set("butler:id", a.join(''))
+    end
+    butler.get("butler:id")
+  end
+  def addressbook
+    @book.each_pair { |k,v|
+      if v.class == Array
+        puts "##{k}: #{v.join(',')}"
+      else
+        puts "@#{k}: #{v}"
+      end
+    }
+  end
+  def daemon ch, &b
+    Process.detach( fork {
+                      butler.subscribe(Base64.urlsafe_encode64(ch)) do |on|
+                        on.message do |ch, msg|
+                          j = JSON.parse(Base64.urlsafe_decode64(msg))
+                          if block_given?
+                            puts b.call(j)
+                          else
+                            puts "[#{j['from']}] #{j['msg']}"
+                          end
+                        end
+                        on.subscribe do |n, i|
+                          puts "+> #{n} #{i}"
+                        end
+                        on.unsubscribe do |n|
+                          puts "-> #{n} #{i}"
+                        end
+                      end
+                    })
+  end
+  def client
+    puts "#### BUTLER ####"
+    addressbook
+    puts "you: #{my_id}"
+    print "to: "
+    t = gets.chomp
+    if t == ''
+      pr = "addressbook"
+      daemon(my_id)
+    elsif /#.+/.match(t)
+      pr = t
+      daemon(t)
+    else
+      pr = t
+      daemon(t)
+    end
+    puts "press Ctrl-C to exit"
+    loop do
+      print "#{pr}> "
+      m = gets.chomp
+      h = { from: my_id, msg: m }
+      if t == ''
+        pr = "addressbook"
+        mm = m.split(' ')
+        if /@.+/.match(mm[0])
+          @book[mm[0].gsub("@", '')] = mm[1]
+          save!
+          addressbook
+        else
+          puts "usage: @nickname id"
+        end
+      else
+        if @book.has_key? t
+          butler.publish(Base64.urlsafe_encode64(@book[t]), Base64.urlsafe_encode64(JSON.generate(h)))
+        else
+          butler.publish(Base64.urlsafe_encode64(t), Base64.urlsafe_encode64(JSON.generate(h)))
+        end
+      end
+    end
+  end
+end
+Butler.new.client
+Signal.trap('INT') { exit 0 }
